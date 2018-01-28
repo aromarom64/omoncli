ee8fb808932f6a9ba8b7509303095c075086cef0
aromarom64 <a.romaschenko@gmail.com>
2018-01-28 16:15:43 +0200

    added sysaux occupants

--- lib/omoncli/tablespace.rb
+++ lib/omoncli/tablespace.rb
@@ -23,6 +23,16 @@ module Omoncli
       vim(sqlplus(r))
     end
 
+    desc "sysaux_occupants", "Displays information about the contents of the SYSAUX tablespace"
+    long_desc <<-LONGDESC
+      omoncli tablespace sysaux_occupants
+    LONGDESC
+    def sysaux_occupants
+      r = Sqlpage.new('template/tablespace/sysaux_occupants.erb')
+      vim(sqlplus(r))
+    end
+
+
   end
 
 end
